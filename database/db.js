c3a319a4cd860c57273385761dc21878631606f6
Maxime Ishimwe <mxmishimwe5@gmail.com>
2022-01-16 07:53:13 +0200

    add env

--- database/db.js
+++ database/db.js
@@ -1,3 +1,6 @@
+const dotEnv = require("dotenv");
+dotEnv.config();
+
 module.exports = {
-  db: "mongodb+srv://savics:Savics1@savics.an43c.mongodb.net/medicalRecordsDatabase?retryWrites=true&w=majority",
+  db: process.env.MONGODB_URI,
 };
